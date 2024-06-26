daea69174ecae7dde735f00dd9679176d72796ff
MiniPakchoi <168171054+MiniPakchoi@users.noreply.github.com>
2024-06-26 20:36:59 +0200

    dann halt so x)

--- 200FizzBuzz/Program.cs
+++ 200FizzBuzz/Program.cs
@@ -1,24 +1,22 @@
 ﻿string userName = Environment.UserName;
 Console.WriteLine($"Hallo {userName} wir spielen eine Runde FizzBuzz");
-int[] zahlen = new int[100];
-for (int i = 0 ; i < zahlen.Length; i++)
+
+for (int i = 1 ; i < 101 ; i++)
 {
-    zahlen[i] = i;
-}
-foreach (int j in zahlen)
-    if (j % 3 == 0 && j % 5 != 0)
+    if (i % 3 == 0 && i % 5 != 0)
     {
         Console.WriteLine("Fizz");
     }
-    else if (j % 3 == 0 && j % 5 == 0)
+    else if (i % 3 == 0 && i % 5 == 0)
     {
         Console.WriteLine("FizzBuzz!");
     }
-    else if (j % 5 == 0 && j % 3 != 0)
+    else if (i % 5 == 0 && i % 3 != 0)
     {
         Console.WriteLine("Buzz");
     }
     else
     {
-    Console.WriteLine(j);
+    Console.WriteLine(i);
     }
+}
