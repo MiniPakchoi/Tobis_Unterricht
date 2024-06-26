30fc771db8cc670de1178c78ce315e452a4e4ba9
MiniPakchoi <168171054+MiniPakchoi@users.noreply.github.com>
2024-06-26 12:31:13 +0200

    mittagspause

--- Dontforget/Program.cs
+++ Dontforget/Program.cs
@@ -1,11 +1,12 @@
 ﻿// Menü anzeigen mit Optionen
 string user = Environment.UserName;
 Console.WriteLine($"Willkommen {user} bei Dontforget");
-Console.WriteLine("Was möchtest du tun?");
-Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen"); 
-
+string[] todos = new string[10];
 do
 {
+    Console.Clear();
+    Console.WriteLine("Was möchtest du tun?");
+    Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen");
     var input = Console.ReadLine();
 
     switch (input)
@@ -13,12 +14,48 @@ do
         case "1":
             //Option 1:Todo erstellen
             //Userinput nehmen
-            Console.WriteLine("Todo erstellen");
             var todo = Console.ReadLine();
+            //Iterieren durch alle Todos (auch die leeren)
+            for (int i = 0; i < todos.Length; i++)
+            {
+                if (todos[i] == null)
+                {
+                    //Fügen wir das Todo hinzu
+                    todos[i] = todo;
+                    Console.WriteLine("Todo erstellt");
+                    break;
+                }
+            }         
             break;
         case "2":
             //Liste der Todos anzeigen
-            Console.WriteLine("Todo anzeigen");
+            Console.WriteLine($"{user} das sind deine Aufgaben:");
+            {
+                for (int i = 0; i < todos.Length; i++)
+                {
+                    Console.WriteLine($"\t{i}.{todos[i]}");
+                }
+                Console.WriteLine("Drücke <ENTER> wenn du ins Menü zurückkehren willst.");
+                Console.ReadKey();
+                break;
+            }
+        case "3":
+            // Löschen
+            Console.WriteLine("Welches Todo möchtest du löschen?");
+            for (int i = 0; i < todos.Length; i++)
+            {
+                Console.WriteLine($"\t{i}. {todos[i]}");
+            }
+            var todoIndexString = Console.ReadLine();
+            int todoIndex = int.Parse(todoIndexString);
+            // float todoIndex = float.Parse(todoIndexString);
+            // int todoIndex1 = Convert.ToInt32(todoIndexString);
+            todos[todoIndex] = null;
             break;
+        case "4":
+            // Updaten/Verändern eines Todos (verändern des strings)
+            // 
+            break;
+
     }
-} while (false);
+} while (true);
