fd1f21d2276ed9852a1aa4a546f373cf3e5ecfd7
MiniPakchoi <168171054+MiniPakchoi@users.noreply.github.com>
2024-06-28 12:01:15 +0200

    Mittagspause

--- Concepts/Collections.cs
+++ Concepts/Collections.cs
@@ -8,7 +8,7 @@ namespace Concepts
 {
     internal class Collections
     {
-        internal void Collection()
+        internal void ArrayRun()
         {
             // Chararrays
             var my_array = "Feierabend!";
@@ -29,6 +29,71 @@ namespace Concepts
             // Array ist eine Liste an Werten
             // Eine kontinuirliche Speicherregion, für mehrere Daten eines gewissen Types
             int[] nums = new int[5];
+        }
+        internal void ListRun()
+        {
+            //Deklaration & Initialisierung
+            //string[] namen = new string[1];
+            //Deklaration
+            string[] namen = new string[2];
+
+            namen[0] = "Thomas";
+            namen[1] = "Susi";
+            //namen[2] = "Markus"
+
+            namen = namen.Append("Makrus").ToArray();
+            //weil voll geht nicht ->passiert was, aber nicht das was wir denken was passieren sollte...
+
+            for (int i = 0; i < namen.Length; i++)
+            {
+                Console.WriteLine(namen[i]);
+            }
+            string[] testArray = { "test", "me" };
+            Console.WriteLine(testArray); //String[]
+
+            //Listen
+            Console.WriteLine("-----Listen----");
+
+            //List<string> hamster = new List<string>();
+            //var hamster = new List<string>();
+            List<string> hamster = new();
+            Console.WriteLine(hamster);
+
+            //Hinzufügen am Ende
+            hamster.Add("Maxi"); //Index 0
+            hamster.Add("Mini"); //Index 1
+
+            //Hinzufügen an gewissen Index
+            hamster.Insert(1, "Mogli");
+
+            Console.WriteLine(hamster[0]);
+
+            //Löschen eines Elements => Angabe des Elements (nicht der Index)
+
+            Console.WriteLine(hamster[1]);
+
+            //Löschen des letzten Elements
+            //hamster.RemoveAT(hamser.Count - 1)
+
+            hamster.Add("Meli");
+
+            if (hamster.Contains("Maxi"))
+            {
+                //mach was cooles!
+            }
+
+            //Anders als bei Array
+            //Liste hat Count, statt Länge!
+            //Count ist die aktuelle Größe der Liste
+            //Length ist die Gesamtkapazität des Arrays
+            //ps:Liste hat eine Kapazität, aber für uns nicht relevant -> hamster.Capacity
+
+            Console.WriteLine(hamster.Count());
+
+            for (int i = 0; i < hamster.Count; i++)
+            {
+                Console.WriteLine(hamster[i]);
+            }
         }
     }
 }
--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -1,62 +1,28 @@
-﻿//Deklaration & Initialisierung
-//string[] namen = new string[1];
-//Deklaration
-string[] namen = new string[2];
+﻿Console.WriteLine("Her mit einer Zahl!");
+var input = Console.ReadLine();
+//int index = int.Parse(input);
+//int index = Convert.ToInt32(input);
+var error = int.TryParse(input, out var index);
 
-namen[0] = "Thomas";
-namen[1] = "Susi";
-//namen[2] = "Markus"
-
-namen = namen.Append("Makrus").ToArray();
-//weil voll geht nicht ->passiert was, aber nicht das was wir denken was passieren sollte...
-
-for (int i = 0; i < namen.Length; i++)
+if (!error)
 {
-    Console.WriteLine(namen[i]);
+    Console.WriteLine(error);
 }
-string[] testArray = { "test", "me" };
-Console.WriteLine(testArray); //String[]
-
-//Listen
-Console.WriteLine("-----Listen----");
-
-//List<string> hamster = new List<string>();
-//var hamster = new List<string>();
-List<string> hamster = new();
-Console.WriteLine(hamster);
-
-//Hinzufügen am Ende
-hamster.Add("Maxi"); //Index 0
-hamster.Add("Mini"); //Index 1
-
-//Hinzufügen an gewissen Index
-hamster.Insert(1, "Mogli");
-
-Console.WriteLine(hamster[0]);
-
-//Löschen eines Elements => Angabe des Elements (nicht der Index)
-
-Console.WriteLine(hamster[1]);
-
-//Löschen des letzten Elements
-//hamster.RemoveAT(hamser.Count - 1)
-
-hamster.Add("Meli");
-
-if (hamster.Contains("Maxi"))
+else
 {
-    //mach was cooles!
+    Console.WriteLine(index);
 }
 
-//Anders als bei Array
-//Liste hat Count, statt Länge!
-//Count ist die aktuelle Größe der Liste
-//Length ist die Gesamtkapazität des Arrays
-//ps:Liste hat eine Kapazität, aber für uns nicht relevant -> hamster.Capacity
-
-Console.WriteLine(hamster.Count());
-
-for (int i = 0; i < hamster.Count; i++)
+int getIndex()
 {
-    Console.WriteLine(hamster[i]);
+    Console.WriteLine("Gebe die Aufgabe ein, die du bearbeiten willst:");
+    var input = Console.ReadLine();
+    var number = int.Parse(input);
+    return number;
 }
+
+string[] my_dontforgets = ["Wäsche waschen", "Bügeln"];
+
+int myIndex = getIndex();
+//int mySecondIndex = getIndex();
+//Console.WriteLine(my_dontforgets[getIndex()]);
