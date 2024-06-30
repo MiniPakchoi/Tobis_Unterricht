b267aa64f61293bd2406909bdcc26680a4f43246
MiniPakchoi <jade.haussmann@gmail.com>
2024-06-30 21:46:50 +0200

    test

--- 200FizzBuzz/Program.cs
+++ 200FizzBuzz/Program.cs
@@ -1,7 +1,54 @@
-﻿string userName = Environment.UserName;
-Console.WriteLine($"Hallo {userName} wir spielen eine Runde FizzBuzz");
+﻿//string userName = Environment.UserName;
+//Console.WriteLine($"Hallo {userName} wir spielen eine Runde FizzBuzz");
 
-for (int i = 1 ; i < 101 ; i++)
+//for (int i = 1 ; i < 101 ; i++)
+//{
+//    if (i % 3 == 0 && i % 5 != 0)
+//    {
+//        Console.WriteLine("Fizz");
+//    }
+//    else if (i % 3 == 0 && i % 5 == 0)
+//    {
+//        Console.WriteLine("FizzBuzz!");
+//    }
+//    else if (i % 5 == 0 && i % 3 != 0)
+//    {
+//        Console.WriteLine("Buzz");
+//    }
+//    else
+//    {
+//    Console.WriteLine(i);
+//    }
+//}
+//string userName = Environment.UserName;
+//Console.WriteLine($"Hallo {userName} wir spielen eine Runde FizzBuzz");
+//int[] zahlen = new int[101];
+//for (int i = 1; i  < zahlen.Length; i++)
+//{
+//    zahlen[i] = i;
+//}
+//foreach (int i in zahlen)
+//    if (i % 3 == 0 && i % 5 != 0)
+//    {
+//        Console.WriteLine("Fizz");
+//    }
+//    else if (i % 3 == 0 && i % 5 == 0)
+//    {
+//        Console.WriteLine("FizzBuzz!");
+//    }
+//    else if (i % 5 == 0 && i % 3 != 0)
+//    {
+//        Console.WriteLine("Buzz");
+//    }
+//    else
+//    {
+//        Console.WriteLine(i);
+//    }
+
+string userName = Environment.UserName;
+Console.WriteLine($"Hallo {userName} wir spielen eine Runde FizzBuzz");
+int[] zahlen = new int[101];
+for (int i = 1; i < zahlen.Length; i++)
 {
     if (i % 3 == 0 && i % 5 != 0)
     {
@@ -17,6 +64,6 @@ for (int i = 1 ; i < 101 ; i++)
     }
     else
     {
-    Console.WriteLine(i);
+        Console.WriteLine(i);
     }
 }
