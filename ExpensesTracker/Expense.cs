ff240703fa291d882974a136adbf33d9434b6e83
MiniPakchoi <jade.haussmann@gmail.com>
2024-07-08 14:39:49 +0200

    case 1 bearbeitet

---
+++ ExpensesTracker/Expense.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpensesTracker
+{
+    internal class Expense
+    {
+        //Properties
+        string Name { get; set; } 
+        decimal Price { get; set; }    
+        string Category { get; set; }
+        DateTime CreatedAt { get; set; }
+
+        public Expense (string name, decimal price, string category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
--- ExpensesTracker/Program.cs
+++ ExpensesTracker/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using ExpensesTracker;
+do
+{
+    Console.WriteLine("-_-_-_-_-Expenses Tracker-_-_-_-_-");
+    Console.WriteLine("Was willst du tun?");
+    Console.WriteLine("\t1. Neue Ausgabe angeben");
+    Console.WriteLine("\t2. Eine Ausgabe löschen");
+    var input = Console.ReadLine(); 
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("neu");
+            Console.WriteLine("Wofür hast du Geld ausgegben?");
+            var  name = Console.ReadLine();
+            Console.WriteLine("Wieviel hast du ausgegeben?");
+            var price = Console.ReadLine();
+            var decimalPrice = decimal.Parse(price);
+            Console.WriteLine("In welche Kategorie willst du dieses Expense einordnen?");
+            var category = Console.ReadLine();
+            var newExpense = new Expense(name, decimalPrice, category);
+            break;
+        case "2":
+            Console.WriteLine("entfernen");
+            break;
+    }
+    Thread.Sleep(2000);
+    Console.Clear();
+} while (true);
