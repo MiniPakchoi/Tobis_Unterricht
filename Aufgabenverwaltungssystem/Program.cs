efd8798e2b379252d421575b540f401f0638d48f
MiniPakchoi <jade.haussmann@gmail.com>
2024-07-09 23:01:20 +0200

    ein satz mit x!

---
+++ Aufgabenverwaltungssystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using Aufgabenverwaltungssystem;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Erstelle mehrere Aufgabenobjekte
+        var task1 = new Task("Dokumentation schreiben", "Schreibe die Projektdokumentation bis zum Ende der Woche", new DateTime(2024, 7, 7));
+        var task2 = new Task("Code-Review durchführen", "Überprüfe den Code des neuen Features", new DateTime(2024, 7, 5));
+
+        // Rufe die Beschreibungen ab und gib sie aus
+        Console.WriteLine(task1.GetTaskInfo());
+        Console.WriteLine(task2.GetTaskInfo());
+
+        // Markiere die erste Aufgabe als erledigt
+        task1.CompleteTask();
+        Console.WriteLine(task1.GetTaskInfo());
+
+        // Verlängere das Fälligkeitsdatum der zweiten Aufgabe
+        task2.ExtendDueDate(new DateTime(2024, 7, 10));
+        Console.WriteLine(task2.GetTaskInfo());
+    }
+}
---
+++ Aufgabenverwaltungssystem/Task.cs
@@ -0,0 +1,38 @@
+﻿namespace Aufgabenverwaltungssystem
+{
+    public class Task
+    {
+        // Eigenschaften kommen hier hin:
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+        // Konstruktor soll hier implementiert werden:
+        public Task(string title, string description, DateTime dueDate)
+        { 
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+            DueDate = DateTime.Now;
+        }
+
+        // Beschreibung der Aufgabe
+        public string GetTaskInfo()
+        {
+            return $"Title: \"{Title}\", Beschreibung: {Description}, Fällig bis: {DueDate}, Erledigt: {IsCompleted}";
+        }
+
+        // Markierung der Aufgabe als erledigt
+        public void CompleteTask()
+        {
+
+        }
+
+        // Verlängerung des Fälligkeitsdatums
+        public void ExtendDueDate(DateTime newDueDate)
+        {
+
+        }
+    }
+}
