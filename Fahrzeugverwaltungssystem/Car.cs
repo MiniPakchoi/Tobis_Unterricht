55e5d0f2e89ce0759be94232731ee5fa9d03fd17
MiniPakchoi <jade.haussmann@gmail.com>
2024-07-09 11:04:29 +0200

    Aufgabe 2

---
+++ Fahrzeugverwaltungssystem/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace Fahrzeugverwaltungssystem
+{
+    internal class Car
+    {
+        internal string Brand;
+        internal string Model;
+        internal int Year;
+        internal int Mileage;
+
+        public Car(string brand,string model, int year, int mileage)
+        { 
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+
+        internal void GetDescription(string description)
+        {
+            Console.WriteLine(Brand + " " + Model + " " + "Jahr:" + Year + " " + "mit" + " " + Mileage + " " + "km gefahren");
+        }
+
+        internal void Drive(int gefahren) 
+        {
+            Console.WriteLine("Wieviel Km bist du gefahren?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int mileage = Mileage;
+            int sumKm = userInput + mileage;
+            Console.WriteLine(sumKm); 
+        }
+
+        internal void Age(int old,string text) 
+        {
+            int year = Year;
+            int actualYear = 2024;
+            int alter = actualYear - year;
+            Console.WriteLine("Dein Auto ist"+ " " +alter + " " + "alt");
+        }
+    }
+}
---
+++ Fahrzeugverwaltungssystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using Fahrzeugverwaltungssystem;
+using System.Net.Mail;
+
+var car1 = new Car("KIA", "Niro", 2019, 62000);
+var car2 = new Car("VW", "Tiguan", 2018, 70000);
+
+car1.GetDescription("Test");
+car1.Drive(12);
+car1.Age(12, " ");
+
+car2.GetDescription("Test");
+car2.Drive(12);
+car2.Age(12, " ");
