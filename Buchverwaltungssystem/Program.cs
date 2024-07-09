eb9f2e4440138a110e7b6fe49cc57d704f783dfc
MiniPakchoi <jade.haussmann@gmail.com>
2024-07-09 14:36:02 +0200

    nochmal korrektur aufgabe 1

--- Buchverwaltungssystem/Program.cs
+++ Buchverwaltungssystem/Program.cs
@@ -2,11 +2,5 @@
 
 var book1 = new Book("Sie nannten micht >Es<", "David Pelzer", 2000, "9783442150557");
 
-book1.Title = "Sie nannten mich >Es<";
-book1.Author = "David Pelzer";
-book1.Publicationyear = 2000;
-book1.Isbn = "9783442150557";
-
-
 Console.WriteLine(book1.GetDescription());
 
