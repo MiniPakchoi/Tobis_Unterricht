137e62e232566730ecea64c6a5f589da936c8868
MiniPakchoi <jade.haussmann@gmail.com>
2024-07-09 09:45:20 +0200

    Aufgabe 1 geschafft ! Wuhu!

---
+++ Buchverwaltungssystem/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Buchverwaltungssystem
+{
+    internal class Book
+    {
+        internal string Title { get; set; }
+        internal string Author { get; set; }
+        internal int Publicationyear { get; set; }
+        internal string Isbn { get; set; }
+
+        public Book(string title, string author, int pulicationYear, string isbn)
+        {
+            Title = title;
+            Author = author;
+            Publicationyear = pulicationYear;
+            Isbn = isbn;
+
+        }
+
+        internal void GetDescription(string description)
+        {
+            Console.WriteLine(Title + " " + Author + "veröffentlicht im Jahr: " + Publicationyear + " " + "ISBN:" + Isbn);
+        }
+    }
+}
---
+++ Buchverwaltungssystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using Buchverwaltungssystem;
+
+var book1 = new Book("Sie nannten micht >Es<", "David Pelzer", 2000, "9783442150557");
+
+book1.Title = "Sie nannten mich >Es<";
+book1.Author = "David Pelzer";
+book1.Publicationyear = 2000;
+book1.Isbn = "9783442150557";
+
+book1.GetDescription("test");
+
